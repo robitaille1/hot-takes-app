d8800daf10d511626c7184beb44d849b3759d096
Lucas Robitaille <lrjames237@gmail.com>
2019-10-16 16:42:06 -0500

    changed some basic styling

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -4,26 +4,40 @@ import Nav from '../Nav/Nav'
 import HomeTakes from '../HomeTakes/HomeTakes'
 import HomeCommentators from '../HomeCommentators/HomeCommentators'
 import './HomePage.css'
+import ApiContext from '../../ApiContext'
 
-export default function HomePage(props) {
-        return (
-            <main className='HomePage'>
-              <Nav />
-              <div className="container">
-                <section className="home-main">
-                    <HomeTakes takes={props.takes}/>
-                    <HomeCommentators commentators={props.commentators}/>
-                </section>
-                <section className="home-add">
-                    <Link to='/add-take' style={{ textDecoration: 'none' }}>
-                        <button className="new-btn">Add New Take</button>
-                    </Link>
-                    <Link to='/add-commentator' style={{ textDecoration: 'none' }}>
-                        <button className="new-btn">Add New Commentator</button>
-                    </Link>
-                </section>
-               </div>
-            </main>
-          );
+export default class HomePage extends React.Component {
+
+    static defaultProps = {
+        match: {
+          params: {}
+        }
+    }
+
+    static contextType = ApiContext
+
+        render() {
+            const { takes=[] } = this.context
+            const { commentators=[] } = this.context
+            return (
+                <main className='HomePage'>
+                  <Nav />
+                  <div className="container">
+                    <section className="home-main">
+                        <HomeTakes takes={takes}/>
+                        <HomeCommentators commentators={commentators}/>
+                    </section>
+                    <section className="home-add">
+                        <Link to='/add-take' style={{ textDecoration: 'none' }}>
+                            <button className="new-btn">Add New Take</button>
+                        </Link>
+                        <Link to='/add-commentator' style={{ textDecoration: 'none' }}>
+                            <button className="new-btn">Add New Commentator</button>
+                        </Link>
+                    </section>
+                   </div>
+                </main>
+              );
+        }
   }
   
