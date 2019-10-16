4145c207736dc524ce6182921dc373534484c8bc
Lucas Robitaille <lrjames237@gmail.com>
2019-10-16 16:38:00 -0500

    created CommentatorPage component

---
+++ src/Components/CommentatorPage/CommentatorPage.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Commentator from '../Commentator/Commentator'
+import ApiContext from '../../ApiContext'
+import { findCommentator, getTakesForCommentator } from '../../commentators-helpers'
+// import CommentatorError from '../CommentatorError/CommentatorError'
+
+export default class CommentatorPage extends React.Component {
+  static defaultProps = {
+    match: {
+      params: {}
+    }
+  }
+  static contextType = ApiContext
+
+  handleDeleteCommentator = commentatorId => {
+    this.props.history.push(`/`)
+  }
+
+  handleDeleteTake = commentatorId => {
+    this.props.history.push(`/`)
+  }
+
+  render() {
+    const { commentators=[] } = this.context
+    const { takes=[] } = this.context
+    const { commentatorId } = this.props.match.params
+    const commentator = findCommentator(commentators, commentatorId) || { content: '' }
+    const commentatorTakes = getTakesForCommentator(takes, commentatorId)
+    return (
+      <section className='Commentator'>
+        {/* <CommentatorError> */}
+            <Commentator
+              id={commentator.id}
+              commentator={commentator}
+              takes={commentatorTakes}
+              onDeleteCommentator={this.handleDeleteCommentator}
+              onDeleteTake={this.handleDeleteTake}
+            />
+        {/* </CommentatorError> */}
+      </section>
+    )
+  }
+}
