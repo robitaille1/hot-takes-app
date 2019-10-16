36cd9f0f1b3b17ee87a661b345153ab2bdd2b8a1
Lucas Robitaille <lrjames237@gmail.com>
2019-10-16 16:40:08 -0500

    added fetch post request functionality

--- src/Components/AddCommentatorForm/AddCommentatorForm.js
+++ src/Components/AddCommentatorForm/AddCommentatorForm.js
@@ -1,47 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ApiContext from '../../ApiContext'
+import config from '../../config'
 import Nav from '../Nav/Nav'
 import './AddCommentatorForm.css'
 
 
-function AddCommentatorForm() {
-    return (
-      <main className='AddCommentatorForm'>
-          <Nav />
-          <section className="container">
-            <form>
-                <fieldset>
-                    <div className="form-section">
-                        <label htmlFor="date">Name:</label>
-                        <input type="text" name="name" id="name" />
-                    </div>
-                    <div className="form-section">
-                        <label htmlFor="commentator">Network:</label>
-                        <select name="Network" id="Network">
-                            <option value="ESPN">ESPN</option>
-                            <option value="Barstool">Barstool Sports</option>
-                            <option value="Ringer">The Ringer</option>
-                            <option value="Fox">Fox</option>
-                            <option value="NBC">NBC</option>
-                        </select>
-                    </div>
-                    <div className="form-section">
-                        <label htmlFor="twitter">Twitter:</label>
-                        <input type="text" />
-                    </div>
-                    <div className="form-section">
-                        <label htmlFor="instagram">Instagram:</label>
-                        <input type="text" />
-                    </div>
-                    <div className="form-section">
-                        <label className="about-label" htmlFor="about">About:</label>
-                        <textarea name="about" id="about" cols="30" rows="10"></textarea>
-                    </div>
-                    <button className="form-btn">Submit</button>
-                </fieldset>
-            </form>
-        </section>
-      </main>
-    );
+export default class AddCommentatorForm extends Component {
+    static contextType = ApiContext
+
+    submitForm = (event) => {
+        event.preventDefault();
+        const newCommentator = {
+            name: event.target['name'].value,
+            network: event.target['network'].value,
+            twitter: event.target['twitter'].value,
+            instagram: event.target['instagram'].value,
+            about: event.target['about'].value,
+        }
+        console.log(newCommentator)
+        fetch(`${config.API_ENDPOINT}/commentators`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCommentator),
+        })
+            .then(response => {
+                if (response.ok)
+                    // return response.json().then(e => Promise.reject(e))
+                    return response.json().then(e => console.log(e))
+            })
+            .then(commentator => {
+                this.context.addCommentator(commentator)
+            })
+            .catch(error => {
+                console.error({ error })
+            })
+    }
+
+
+    render() {
+        return (
+            <main className='AddCommentatorForm'>
+                <Nav />
+                <section className="container">
+                  <form onSubmit={this.submitForm}>
+                      <fieldset>
+                          <div className="form-section">
+                              <label htmlFor="date">Name:</label>
+                              <input type="text" name="name" id="name" />
+                          </div>
+                          <div className="form-section">
+                              <label htmlFor="commentator">Network:</label>
+                              <input type="text" name="network" id="network" />
+                          </div>
+                          <div className="form-section">
+                              <label htmlFor="twitter">Twitter:</label>
+                              <input type="text" name="twitter" id="twitter"/>
+                          </div>
+                          <div className="form-section">
+                              <label htmlFor="instagram">Instagram:</label>
+                              <input type="text" name="instagram" id="instagram" />
+                          </div>
+                          <div className="form-section">
+                              <label className="about-label" htmlFor="about">About:</label>
+                              <textarea name="about" id="about" cols="30" rows="10"></textarea>
+                          </div>
+                          <button className="form-btn">Submit</button>
+                      </fieldset>
+                  </form>
+              </section>
+            </main>
+          );
+    }
   }
   
-export default AddCommentatorForm;
--- src/Components/AddTakeForm/AddTakeForm.js
+++ src/Components/AddTakeForm/AddTakeForm.js
@@ -1,54 +1,92 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ApiContext from '../../ApiContext'
+import { findCommentatorId } from '../../commentators-helpers'
+import config from '../../config'
 import Nav from '../Nav/Nav'
 import './AddTakeForm.css'
 
 
-function AddTakeForm() {
-    return (
-      <main className='AddTakeForm'>
-          <Nav />
-          <section className="container">
-            <form>
-                <fieldset>
-                    <div className="form-section">
-                        <label htmlFor="date">Date:</label>
-                        <input type="date" name="date" id="date" />
-                    </div>
-                    <div className="form-section">
-                        <label htmlFor="commentator">Select a Commentator:</label>
-                        <select name="commentator" id="commentator">
-                            <option value="Bill">Bill Simmons</option>
-                            <option value="Dave">Dave Portnoy</option>
-                            <option value="Stephen">Stephen A. Smith</option>
-                        </select>
-                    </div>
-                    <div className="form-section">
-                        <label className="take-label"htmlFor="take">Take Information:</label>
-                        <textarea name="take" id="take" cols="30" rows="10"></textarea>
-                    </div>
-                    <div className="form-section">
-                        <label htmlFor="sport">Sport:</label>
-                        <select name="sport" id="Sport">
-                            <option value="NBA">NBA</option>
-                            <option value="NFL">NFL</option>
-                            <option value="MLB">MLB</option>
-                            <option value="NHL">NHL</option>
-                        </select>
-                    </div>
-                    <div className="form-section">
-                        <label htmlFor="correct">Correct or Incorrect?</label>
-                        <select name="correct" id="correct">
-                            <option value="true">True</option>
-                            <option value="false">False</option>
-                            <option value="pending">Pending</option>
-                        </select>
-                    </div>
-                    <button className="form-btn">Submit</button>
-                </fieldset>
-            </form>
-        </section>
-      </main>
-    );
+export default class AddTakeForm extends Component {
+    static contextType = ApiContext;
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const { commentators=[] } = this.context
+        const idForCommentator = findCommentatorId(commentators, event.target['commentator'].value);
+        const newTake = {
+          take: event.target['take'].value,
+          date: event.target['date'].value,
+          commentatorid: idForCommentator.id,
+          commentator: event.target['commentator'].value,
+          correct: event.target['correct'].value,
+          sport: event.target['sport'].value,
+        }
+        fetch(`${config.API_ENDPOINT}/takes`, {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify(newTake),
+        })
+          .then(res => {
+            if (!res.ok)
+              return res.json().then(event => Promise.reject(event))
+            return res.json()
+          })
+          .then(take => {
+            this.context.addTake(take)
+          })
+          .catch(error => {
+            console.error({ error })
+          })
+    }
+
+    render() {
+      const { commentators=[] } = this.context
+        return (
+            <main className='AddTakeForm'>
+                <Nav />
+                <section className="container">
+                  <form onSubmit={this.handleSubmit}>
+                      <fieldset>
+                          <div className="form-section">
+                              <label htmlFor="date">Date:</label>
+                              <input type="date" name="date" id="date" />
+                          </div>
+                          <div className="form-section">
+                              <label htmlFor="commentator">Select a Commentator:</label>
+                              <select name="commentator" id="commentatorName">
+                                {commentators.map(commentator =>
+                                  <option key={commentator.id} value={commentator.name}>{commentator.name}</option>
+                                )}
+                              </select>
+                          </div>
+                          <div className="form-section">
+                              <label className="take-label"htmlFor="take">Take Information:</label>
+                              <textarea name="take" id="take" cols="30" rows="10"></textarea>
+                          </div>
+                          <div className="form-section">
+                              <label htmlFor="sport">Sport:</label>
+                              <select name="sport" id="Sport">
+                                  <option value="NBA">NBA</option>
+                                  <option value="NFL">NFL</option>
+                                  <option value="MLB">MLB</option>
+                                  <option value="NHL">NHL</option>
+                              </select>
+                          </div>
+                          <div className="form-section">
+                              <label htmlFor="correct">Correct or Incorrect?</label>
+                              <select name="correct" id="correct">
+                                  <option value="TRUE">TRUE</option>
+                                  <option value="FALSE">FALSE</option>
+                              </select>
+                          </div>
+                          <button className="form-btn">Submit</button>
+                      </fieldset>
+                  </form>
+              </section>
+            </main>
+          );
+    }
+    
   }
   
-  export default AddTakeForm;
