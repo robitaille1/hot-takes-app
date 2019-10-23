22efb3721e9c468e3df70237850762eb1d66d05c
Lucas Robitaille <lrjames237@gmail.com>
2019-10-23 09:15:59 -0500

    added commentatorError test edited index.html site description

---
+++ src/Components/CommentatorError/CommentatorError.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import CommentatorError from './CommentatorError'
+
+describe(`CommentatorError Component`, () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <Router><CommentatorError /></Router>, div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  })
+})
